92a21481d818a4693aec651fe1c6c5a1b519ba05
Cptn Reef <sharif.staffordbz@gmail.com>
2020-09-26 11:44:35 -0400

    Question 1-4 update

---
+++ List_Loops.py
@@ -0,0 +1,23 @@
+#Question 1
+songs = ["ROCKSTAR", "Do It", "For The Night"]
+
+print("\n") #Question 2
+print(songs[0])
+print(songs[2])
+print(songs[1:3])
+
+print("\n") #Question 3
+songs[2] = "Shake It Off!"
+print(songs)
+
+print("\n") #Question 4
+songs.append("Lasers")
+print(songs)
+songs.extend(["Hold On", "Fergalicious"])
+print(songs)
+del songs[5]
+print(songs)
+
+print("\n") #Question 5
+
+print("\n") #Question 6
